c0431b621d7b1445331f3281a316d55e52341982
Alex Tsirozidis <saltypickles@yahoo.com>
2018-01-26 00:43:08 -0600

    Done

--- pixiedust.js
+++ pixiedust.js
@@ -3,21 +3,24 @@
     const pixieDust = document.body.querySelector("#pixiedust");
 
     if (pixieDust !== null) {
-
+      
         if (autopixie_status===true) {
             chrome.storage.local.get('pixiescript', ret => {
                 pixieDust.value = ret.pixiescript;
+                pixieDust.setAttribute("style","background-color:#FFFFE7; width: 80%; height:80%");
+                alert ("See my pixie dust!");
             });
         } else {
             chrome.storage.local.set({
                 'pixiescript': pixieDust.value
               });
+              pixieDust.value='';
+              pixieDust.setAttribute("style","background-color:white; width: 20%; height:20%");
+              alert ("Thanks for the new pixie dust!");
         }
 
-
-
-
-
+    } else {
+        location.href = location.href;
     }
 
 
--- program.js
+++ program.js
@@ -2,7 +2,19 @@
 
 
     chrome.storage.local.get('pixiescript', ret => {
-        eval(ret.pixiescript);
+        if (ret.pixiescript.length > 1) {
+            eval(ret.pixiescript);
+        } else 
+        {
+            var toolbar = document.body.querySelector(".toolbar"); 
+            if (toolbar !== null) toolbar.style.display = 'none';
+
+            document.body.setAttribute("style","padding-top:0px");
+
+            var errorMsg = document.body.querySelector(".messages--error");
+            if (errorMsg !== null) errorMsg .style.display = 'none';
+        }
+        
     });
 
 
